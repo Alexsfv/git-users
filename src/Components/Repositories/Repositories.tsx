d8cfab425e000f3ac7678a82e60eefc23d461907
Alexsfv <safronov.sanya37@gmail.com>
2021-01-05 19:53:42 +0500

    firebase

--- src/Components/Repositories/Repositories.tsx
+++ src/Components/Repositories/Repositories.tsx
@@ -4,14 +4,13 @@ import { useStore } from '../../store/StoreContext'
 import RepoCard from '../RepoCard/RepoCard'
 
 const Repositories: React.FC<{}> = observer(() => {
-
     const { repositories } = useStore()
     const repos_count = repositories.repos_count
 
     return (
         <div className="row py-4 bg-primary rounded">
             <div className="container">
-                <div className="d-flex flex-wrap">
+                <div className="d-flex flex-wrap justify-content-around">
                     <p className="w-100 font-weight-bold text-white text-center" style={{fontSize: '20px'}}>Список репозиториев</p>
                     {
                         repos_count
